486de280121148a855ff1cc66454faa72f25c4bd
TheBoomBoxer <alexj.27.97@gmail.com>
2017-06-03 09:30:16 +0200

    Physics Included
    Floor has now physics and the start point is on the track.

--- src/mygame/Game.java
+++ src/mygame/Game.java
@@ -1,6 +1,8 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.light.DirectionalLight;
 import com.jme3.light.Light;
 import com.jme3.math.Vector3f;
@@ -14,6 +16,9 @@ import com.jme3.scene.Geometry;
  */
 
 public class Game extends SimpleApplication {
+    
+    private BulletAppState physical_states;
+    private RigidBodyControl physics_track;
 
     public static void main(String[] args) {
         Game app = new Game();
@@ -22,6 +27,9 @@ public class Game extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        flyCam.setMoveSpeed(8f);
+        physical_states = new BulletAppState();
+        stateManager.attach(physical_states);
         createTrack();
     }
     
@@ -29,6 +37,11 @@ public class Game extends SimpleApplication {
         Geometry track = (Geometry) assetManager.loadModel("Models/track.j3o");
         Light light = new DirectionalLight(Vector3f.UNIT_Y.mult(-1));
         track.addLight(light);
+        
+        physics_track = new RigidBodyControl(0f);
+        track.addControl(physics_track);
+        physical_states.getPhysicsSpace().add(physics_track);
+        physics_track.setFriction(.5f);
         rootNode.attachChild(track);
     }
 
